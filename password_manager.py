62cf25e84ba44e42965d645b8a05ce9ed94db033
Kelvin Ziro <kelvin.ziroh@gmail.com>
2024-02-17 10:17:15 +0300

    Store passwords for various accounts

---
+++ password_manager.py
@@ -0,0 +1,10 @@
+#! python3
+# password_manager.py - An insecure password manager program
+
+# Store the passords for various accounts
+PASSWORDS = {
+    'email': ']"~9!k3-L.Lgj22)cMtp"`m/N3',
+    'blog': 'j9J&-?FVk[jR;epaBpu&5$K7G#',
+    'luggage': 'Fd,3<W"mebb!ZA!,r|6?)ytU!)',
+}
+
